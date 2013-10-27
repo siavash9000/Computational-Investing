df7bc06598aa2886c5e1ef3c7b8af36730dda332
siavash9000 <siavash@siavash-ThinkPad-X230.(none)>
2013-10-27 18:07:02 +0100

    finished marketsim, not verified yet

--- HomeWork3/marketsim.py
+++ HomeWork3/marketsim.py
@@ -11,6 +11,7 @@ import pandas
 import copy
 import QSTK.qstkutil.tsutil as tsutil
 import csv
+import sys
 
 print "Pandas Version", pandas.__version__
 
@@ -24,12 +25,15 @@ def extract_dates_and_symbols(filename):
     for row in reader:
         date = datetime.datetime(int(row[0]), int(row[1]), int(row[2]), 16)
         dates.append(date)
-        symbols.append(row[3])
+        symbol = row[3]
+        symbols.append(symbol)
+        amount = row[5]
         if row[4] == 'Buy':
-            orders.append([date, row[3], (int(row[5]))])
+            orders.append([date, symbol, (int(amount))])
         else:
-            orders.append([date, row[3], (-int(row[5]))])
+            orders.append([date, symbol, (-int(amount))])
 
+    symbols = list(set(symbols))
     return dates, symbols, orders
 
 
@@ -48,31 +52,54 @@ def get_stockdata(startdate, enddate, stocks):
     return stockdata, tradingdays
 
 
-def create_tradetmatrix(dates, symbols):
-    starttime = min(dates)
-    endtime = max(dates) + datetime.timedelta(days=1)
-    symbols_without_duplicates = copy.deepcopy(symbols)
-    symbols_without_duplicates = list(set(symbols_without_duplicates))
-    stockdata, tradingdays = get_stockdata(starttime, endtime, symbols_without_duplicates)
-    tradematrix = pandas.DataFrame(stockdata, index=tradingdays, columns=symbols_without_duplicates)
+def create_tradematrix(stockdata, tradingdays, symbols):
+    tradematrix = pandas.DataFrame(stockdata, index=tradingdays, columns=symbols)
     #init tradematrix with zeros
     for day in tradingdays:
-        for symbol in symbols_without_duplicates:
+        for symbol in symbols:
             tradematrix[:day][symbol] = 0
     return tradematrix
 
 
 def addorders(tradematrix, dates, symbols, orders):
     for order in orders:
-        tradematrix[order[0]:order[0]][order[1]] = order[2]
+        current = tradematrix[order[0]:tradematrix.index[-1]][order[1]]
+        tradematrix[order[0]:tradematrix.index[-1]][order[1]] = current + order[2]
+
+
+def create_cashseries(tradingdays, startcash, orders, prices):
+    cash = pandas.Series(0, index=tradingdays)
+    cash[0:-1] = startcash
+    for day in tradingdays:
+       for order in orders:
+            if order[0] == day:
+                cash[day:day] = cash[day:day] - prices[day:day][order[1]]*order[2]
+                cash[day:tradingdays[-1]] = cash[day:day]
+    return cash
 
 
 def main():
-    dates, symbols, orders = extract_dates_and_symbols("orders.csv")
-    tradematrix = create_tradetmatrix(dates, symbols)
+    startcash = int(sys.argv[1])
+    order_filename = sys.argv[2]
+    outputfilename = sys.argv[3]
+    dates, symbols, orders = extract_dates_and_symbols(order_filename)
+    starttime = min(dates)
+    endtime = max(dates) + datetime.timedelta(days=1)
+    stockdata, tradingdays = get_stockdata(starttime, endtime, symbols)
+    prices = stockdata['close']
+    adjustedprices = stockdata['actual_close']
+    tradematrix = create_tradematrix(stockdata, tradingdays, symbols)
     addorders(tradematrix, dates, symbols, orders)
-    print tradematrix
-
+    cash = create_cashseries(tradingdays, startcash, orders, prices)
+    adjustedprices['_CASH'] = 1.0
+    tradematrix['_CASH'] = cash
+    tradematrix = tradematrix*adjustedprices
+    tradematrix['_VALUE'] = 0.0
+    for day in tradematrix.index:
+        for symbol in symbols:
+            tradematrix[day:day]['_VALUE'] = tradematrix[day:day]['_VALUE'] + tradematrix[day:day][symbol]
+        tradematrix[day:day]['_VALUE'] = tradematrix[day:day]['_VALUE'] + tradematrix[day:day]['_CASH']
+        print tradematrix[day:day]
 
 if __name__ == '__main__':
     main()
