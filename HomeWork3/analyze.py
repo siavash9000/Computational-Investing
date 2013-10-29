2250d811524d4d8e74f85b45f22e055a05d2ef02
siavash9000 <siavash@siavash-ThinkPad-X230.(none)>
2013-10-29 21:57:17 +0100

    added simple visualization

--- HomeWork3/analyze.py
+++ HomeWork3/analyze.py
@@ -64,8 +64,45 @@ def main():
     benchmark_symbol = sys.argv[2]
     dates, values = extract_dates_and_values(values_filename)
     print simulate(values)
+    import datetime
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib.dates as mdates
+    import matplotlib.cbook as cbook
+
+    years    = mdates.YearLocator()   # every year
+    months   = mdates.MonthLocator()  # every month
+    yearsFmt = mdates.DateFormatter('%Y')
+
+    # load a numpy record array from yahoo csv data with fields date,
+    # open, close, volume, adj_close from the mpl-data/example directory.
+    # The record array stores python datetime.date as an object array in
+    # the date column
+
+    fig, ax = plt.subplots()
+    ax.plot(dates, values)
 
 
+    # format the ticks
+    ax.xaxis.set_major_locator(years)
+    ax.xaxis.set_major_formatter(yearsFmt)
+    ax.xaxis.set_minor_locator(months)
+
+    datemin = datetime.date(dates[0].year, 1, 1)
+    datemax = datetime.date(dates[-1].year+1, 1, 1)
+    ax.set_xlim(datemin, datemax)
+
+    # format the coords message box
+    def price(x): return '$%1.2f'%x
+    ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+    ax.format_ydata = price
+    ax.grid(True)
+
+    # rotates and right aligns the x labels, and moves the bottom of the
+    # axes up to make room for them
+    fig.autofmt_xdate()
+
+    plt.show()
 if __name__ == '__main__':
     main()
 
